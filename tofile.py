9f22c2ab5b4f1f99fa94e4bdf4a1a1aae0fde8a7
sunjangyo12 <sunjangyo12@gmail.com>
2025-07-30 17:01:20 +0700

    fix and usage: extrack zip from chatgpt > copy index.html and tofile.py > here > mkdir data > python tofile.py

--- tofile.py
+++ tofile.py
@@ -1,23 +1,45 @@
+
 import json
 import os
 
+
 conditionals = None
+outhtml = None
 
 with open("conversations.json", "rb") as f:
    conditionals = json.load(f)
+
+with open("index.html", "r") as f:
+   outhtml = f.read()
 
 
 count = 1
 
 for jsonData in conditionals:
-   title = jsonData['title']
+   title = jsonData["title"]
    dest = f"data/{count}. {title}" #title belum difilter, error jika ada karakter unik
    os.mkdir(dest)
 
-   with open(f"{dest}/data.json", 'wb') as f:
-      f.write(bytes(f"{jsonData}\n", encoding='utf-8'))
+   jdata = json.dumps(jsonData)
+
+   with open(f"{dest}/jsondata.js", "w") as f:
+      f.write(f"var outjson = [{jdata}]")
+
+   with open(f"{dest}/index.html", "w") as f:
+      f.write(outhtml)
 
    print(f"[+] process: {count} done.")
    count += 1
+
 #   break
+
+
+
+
+
+
+
+
+
+
 
